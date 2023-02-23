7ff0a4303f98e4f0fc315ef49458f0179a0d7954
Javi <javiquintanarodriguez@gmail.com>
2023-02-23 23:31:54 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let navMobile = document.getElementById("expand-nav");
+
+function expandMenu(){
+    navMobile.classList.add("active");
+}
+function closeMenu(){
+    navMobile.classList.remove("active");
+}
